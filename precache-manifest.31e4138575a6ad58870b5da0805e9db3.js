2ef476970a79e9a38ec422d53dc05b09b0d14563
Isaac Cui <cuizixuan@easternbay.cn>
2020-08-12 12:00:26 +0800

    Updates

---
+++ precache-manifest.31e4138575a6ad58870b5da0805e9db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a358db534d2eb78ae396293cb6a4e0",
+    "url": "/maple-helper/index.html"
+  },
+  {
+    "revision": "371017b380050bd299cc",
+    "url": "/maple-helper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a0829f8b6821e052909",
+    "url": "/maple-helper/static/js/2.13b0f4f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maple-helper/static/js/2.13b0f4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371017b380050bd299cc",
+    "url": "/maple-helper/static/js/main.6430a9cc.chunk.js"
+  },
+  {
+    "revision": "b56ae4f894683b4f14a5",
+    "url": "/maple-helper/static/js/runtime-main.760432ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maple-helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
